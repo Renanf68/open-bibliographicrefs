b631f9523e52665fc6b4aefddff15b5178e56478
RenanC <renancostam@gmail.com>
2021-08-20 10:55:50 -0300

    CustomToast redesign

--- src/components/CustomToast.tsx
+++ src/components/CustomToast.tsx
@@ -1,42 +1,58 @@
 import { Box, HStack, Icon, Text, useToast } from '@chakra-ui/react';
-import { MdClose, MdErrorOutline } from 'react-icons/md'
+import { MdClose, MdErrorOutline, MdHighlightOff } from 'react-icons/md';
+import { FaRegCheckCircle } from 'react-icons/fa';
+import React from 'react';
 
-interface CustomToastProps {
+interface MessageType {
   type: 'success' | 'warning' | 'error';
+};
+
+const MessageIcon = ({ type }: MessageType) => {
+  if(type === 'warning') return <Icon as={MdErrorOutline} />
+  if(type === 'error') return <Icon as={MdHighlightOff} />
+  return (
+    <Icon as={FaRegCheckCircle} />
+  );
+};
+
+interface CustomToastProps extends MessageType {
   message: string;
-}
+};
 
 export const CustomToast = ({ type, message }: CustomToastProps) => {
   // contex
   const toast = useToast();
   // helpers
-  let bg = '#48BB78';
-  if (type === 'warning') bg = '#ED8936';
-  if (type === 'error') bg = '#E53E3E';
+  let bg = '#F3FFF6';
+  if (type === 'warning') bg = '#FFF6E8';
+  if (type === 'error') bg = '#FFF3F3';
+  let color = '#00AC26'
+  if (type === 'warning') color = '#E28800';
+  if (type === 'error') color = '#D70000';
   // UI
   return (
     <Box
       pos="relative"
-      color="white"
+      color={color}
       p="4"
       bg={bg}
       borderRadius="lg"
       w={{ base: '100%', md: 'auto' }}
       maxW={{ base: '100vw', md: 'auto' }}
+      fontSize="sm"
+      fontWeight="medium"
     >
-      <Icon
-        as={MdClose}
-        pos="absolute"
-        top="2"
-        right="3"
-        cursor="pointer"
-        onClick={() => toast.closeAll()}
-      />
-      <HStack spacing={4} pr="6">
-        <Icon as={MdErrorOutline} />
+      <HStack spacing={4}>
+        <MessageIcon type={type} />
         <Box>
           <Text>{message}</Text>
         </Box>
+        <Icon
+          as={MdClose}
+          cursor="pointer"
+          color="#AEAEAE"
+          onClick={() => toast.closeAll()}
+        />
       </HStack>
     </Box>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,54 +15,54 @@ export default function Home() {
   // UI
   return (
     <Box>
-    <Head>
-      <title>Referências bibliográficas</title>
-    </Head>
-    <Header />
-    <Center minH="100vh" pt="100px" pb={searchResponse ? '12' : '8'}>
-      <Container>
-        <Flex flexDir="column" justifyContent="center">
-          <Heading as="h1" fontSize="lg" lineHeight="lg" textAlign="center">
-            Referências bibliográficas
-          </Heading>
-          <Heading 
-            mt="4" 
-            as="h2" 
-            fontSize="sm" 
-            fontWeight="normal"
-            lineHeight="sm" 
-            textAlign="center" 
-          >
-            Ferramenta Open Source de apoio à comunidade acadêmica.
-          </Heading>
-          <Flex mt="8" w="100%" p="24px" bg="purple.200" borderRadius="lg">
-            <Box mr="4">
-              <Icon as={MdErrorOutline} color="purple.600" /> 
-            </Box>
-            <Text fontSize="sm" lineHeight="md">
-              Informe o 
-              <Tooltip 
-                label="A sigla DOI significa Digital Object Identifier (Identificador de Objeto Digital). Mais informação em https://www.doi.org/" 
-                aria-label="Identificador de Objeto Digital"
-              >
-                <strong>{' DOI'}</strong>
-              </Tooltip> e busque pela correspondente referência bibliográfica de artigos, 
-                teses, dissertações, livros e capítulos de livros.
-            </Text>
+      <Head>
+        <title>Referências bibliográficas</title>
+      </Head>
+      <Header />
+      <Center minH="100vh" pt="100px" pb={searchResponse ? '12' : '8'}>
+        <Container>
+          <Flex flexDir="column" justifyContent="center">
+            <Heading as="h1" fontSize="lg" lineHeight="lg" textAlign="center">
+              Referências bibliográficas
+            </Heading>
+            <Heading 
+              mt="4" 
+              as="h2" 
+              fontSize="sm" 
+              fontWeight="normal"
+              lineHeight="sm" 
+              textAlign="center" 
+            >
+              Ferramenta Open Source de apoio à comunidade acadêmica.
+            </Heading>
+            <Flex mt="8" w="100%" p="24px" bg="purple.200" borderRadius="lg">
+              <Box mr="4">
+                <Icon as={MdErrorOutline} color="purple.600" /> 
+              </Box>
+              <Text fontSize="sm" lineHeight="md">
+                Informe o 
+                <Tooltip 
+                  label="A sigla DOI significa Digital Object Identifier (Identificador de Objeto Digital). Mais informação em https://www.doi.org/" 
+                  aria-label="Identificador de Objeto Digital"
+                >
+                  <strong>{' DOI'}</strong>
+                </Tooltip> e busque pela correspondente referência bibliográfica de artigos, 
+                  teses, dissertações, livros e capítulos de livros.
+              </Text>
+            </Flex>
+            <ToolBox />
+            {Object.keys(searchResponse).length > 0 && standard === "abnt" && (
+              <SearchByDoiResultABNT data={searchResponse} />
+            )}
+            {Object.keys(searchResponse).length > 0 && standard === "apa" && (
+              <SearchByDoiResultAPA data={searchResponse} />
+            )}
+            {Object.keys(searchResponse).length > 0 && standard === "vancouver" && (
+              <SearchByDoiResultVanc data={searchResponse} />
+            )}
           </Flex>
-          <ToolBox />
-          {Object.keys(searchResponse).length > 0 && standard === "abnt" && (
-            <SearchByDoiResultABNT data={searchResponse} />
-          )}
-          {Object.keys(searchResponse).length > 0 && standard === "apa" && (
-            <SearchByDoiResultAPA data={searchResponse} />
-          )}
-          {Object.keys(searchResponse).length > 0 && standard === "vancouver" && (
-            <SearchByDoiResultVanc data={searchResponse} />
-          )}
-        </Flex>
-      </Container>
-    </Center>
+        </Container>
+      </Center>
     </Box>
   )
 }
